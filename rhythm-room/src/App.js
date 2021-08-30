1b053e9728ee8c3990f2d6329deff022bb0004c8
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2021-08-30 10:40:12 -0700

    iframe

--- rhythm-room/src/App.js
+++ rhythm-room/src/App.js
@@ -5,8 +5,6 @@ import { Container, Button} from 'react-bootstrap';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/navbar';
-import Footer from './components/footer';
-
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -16,22 +14,6 @@ import Dashboard from './pages/Dashboard';
 import styles from "./App.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import music_design from './assets/music_design.svg'
-
-/*
-function App() {
-  return (
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route path="/" exact component={HomePage}/>
-        <Route path="/about" component={AboutPage}/>
-        <Route path="/dashboard" component={Dashboard}/>
-      </Switch>
-      <Footer/>
-    </Router>
-  );
-}
-*/
 
 class App extends Component {
   state = {
--- rhythm-room/src/pages/Dashboard.js
+++ rhythm-room/src/pages/Dashboard.js
@@ -1,17 +1,80 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+
+class Dashboard extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        controls: 0,
+        disablekb: 1,
+      },
+    };
+
+    return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+  }
+}
+
+export default Dashboard;
+
+
+/*
+import React from 'react';
 import { Container, Button} from 'react-bootstrap';
 
 import styles from "./Dashboard.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+      playerVars: {
+      'playsinline': 1
+      },
+      events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+      }
+  });
+}
+
 function Dashboard() {
+
   return (
     <div>
       <Container>
         Dashboard
+
+        <br></br>
+        <div id="player"></div> hi
+        <script></script>
+
       </Container>
     </div>
+    
   );
 }
 
 export default Dashboard;
+
+        <iframe id="player" type="text/html" width="640" height="390"
+          src="http://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1"
+          frameborder="0">
+        </iframe>
+*/
