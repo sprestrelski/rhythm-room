53762be418338104d9302c73f13a27817e08c28d
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2021-09-01 13:51:36 -0700

    change video playing based on link

--- rhythm-room/src/pages/Dashboard.js
+++ rhythm-room/src/pages/Dashboard.js
@@ -1,86 +1,72 @@
-import React from 'react';
-import YouTube from 'react-youtube';
-
-class Dashboard extends React.Component {
-  render() {
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-        controls: 0,
-        disablekb: 1,
-      },
-    };
-
-    const videoId = "2g811Eo7K8U"
-
-    return (
-      <div>
-        <YouTube videoId={videoId} opts={opts} onReady={this._onReady} />
-      </div>
-    );
-  }
-
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-  }
-}
-
-export default Dashboard;
-
-
-/*
-import React from 'react';
+import React from "react";
+import YouTube from "react-youtube";
+import Modal from "react-modal";
 import { Container, Button} from 'react-bootstrap';
 
-import styles from "./Dashboard.module.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+const modalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
 
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: 'M7lc1UVf-VE',
-      playerVars: {
-      'playsinline': 1
-      },
-      events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-      }
-  });
-}
-
+// Render function for Prismic headless CMS pages
 function Dashboard() {
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+  const [videoUrl, setVideoUrl] = React.useState("");
+  let videoCode;
+  if (videoUrl) {
+    try{
+      videoCode = videoUrl.split("v=")[1].split("&")[0];
+      console.log("videoURL: " + videoCode);
+    }
+    catch (error){
+      videoCode = videoUrl;
+      console.log("ur BAD");
+    }
+    
+  }
+
+  const checkElapsedTime = (e) => {
+    console.log(e.target.playerInfo.playerState);
+    const duration = e.target.getDuration();
+    const currentTime = e.target.getCurrentTime();
+  };
+
+  const opts = {
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  };
 
   return (
     <div>
       <Container>
-        Dashboard
-
-        <br></br>
-        <div id="player"></div> hi
-        <script></script>
-
+        <div>
+          <h1>Dashboard</h1>
+          <div></div>
+        </div>
+        <div>
+        <label htmlFor="songURL">Enter Song URL: </label>
+        <input id="songURL" value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} />
+        
+          <div>
+            <YouTube
+              videoId={videoCode}
+              containerClassName="embed embed-youtube"
+              onStateChange={(e) => checkElapsedTime(e)}
+              opts={opts}
+            />
+          </div>
+        </div>
       </Container>
     </div>
-
   );
 }
 
 export default Dashboard;
-
-        <iframe id="player" type="text/html" width="640" height="390"
-          src="http://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1"
-          frameborder="0">
-        </iframe>
-*/
