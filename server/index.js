720f5e83c00d69856fecd261e6f9225d6cca40d4
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2021-09-02 11:21:25 -0700

    Fix disconnect websockets

--- server/index.js
+++ server/index.js
@@ -5,57 +5,58 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection <3');
-    socket.on('join', ({name, room}, callback) => {
-        const { error, user } = addUser( { id: socket.id, name, room});
-        if(error) return callback(error)
-        socket.emit('message', { user: 'admin', text: `${user.name} has joined #${user.room}`});
-        console.log(`${user.name} has joined #${user.room}`)
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined #${user.room}`})
-        socket.join(user.room);
+  console.log('We have a new connection <3');
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error)
+    socket.emit('message', { user: 'admin', text: `${user.name} has joined #${user.room}` });
+    console.log(`${user.name} has joined #${user.room}`)
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined #${user.room}` })
+    socket.join(user.room);
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
 
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
+    callback();
+  });
 
-        callback();
-    });
+  socket.on('videoUrl', (videoUrl, callback) => {
+    const user = getUser(socket.id);
+    console.log(videoUrl)
+    io.to(user.room).emit('videoUrl', videoUrl)
+    console.log("test")
+    callback();
+  });
 
-    socket.on('videoUrl', (videoUrl, callback) => {
-      const user = getUser(socket.id);
-      console.log(videoUrl)
-      io.to(user.room).emit('videoUrl', videoUrl)
-      console.log("test")
-      callback();
-    });
-    
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-        callback();
-      });
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    callback();
+  });
 
-      socket.on('', (message, callback) => {
-        const user = getUser(socket.id);
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-        callback();
-      });
+  socket.on('', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    callback();
+  });
 
-    socket.on('disconnect', () => {
-        const user = getUser(socket.id);
-        socket.emit('message', { user: 'admin', text: `${user.name} has left #${user.room}`});
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left #${user.room}`})
-        console.log('User left </3');
-    })
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    }
+  })
 });
 
 app.use(router);
